f4ef80102accecc48c4f6bfaea1604c252cbc126
Edoardo Frati <efrati@kpmg.it>
2025-07-29 11:10:57 +0200

    test 2

--- unit-test/server.test.js
+++ unit-test/server.test.js
@@ -1,11 +1,12 @@
 const request = require('supertest');
-const app = require('../index.js'); // percorso al file server.js
+const assert = require('assert');
+const app = require('../index.js')
 
 describe('GET /', () => {
     it('should return 200 and contain expected text', async () => {
         const response = await request(app).get('/');
-        expect(response.statusCode).toBe(200);
-        expect(response.text).toContain('<h1>Hello World!</h1>');
-        expect(response.text).toContain('Benvenuto sulla tua pagina Node.js');
+        assert.strictEqual(response.statusCode, 200);
+        assert.match(response.text, /<h1>Hello World!<\/h1>/);
+        assert.match(response.text, /Benvenuto sulla tua pagina Node.js/);
     });
 });
